139cd209d32d25daa0604f1c63d8942090b2c475
yhw2003 <321612813@qq.com>
2023-10-24 20:17:12 +0800

    简单web，状态保存和恢复

--- 01-tutorial/02-a_simple_software.py
+++ 01-tutorial/02-a_simple_software.py
@@ -1,16 +1,36 @@
 from flask import Flask
+import sys
+
 
 app = Flask(__name__)
+try:
+    global data_obj
+    data_obj = open("./data.obj", 'r+')
+except:
+    data_obj = open("./data.obj", 'w+')
 
-num = 0
+
+try:
+    global num
+    num = int(data_obj.read())
+except:
+    num = 0
 
 @app.route("/")
 def numH(): 
-    global num
-    num += 1
+    num = getNum()
     return {"msg": num}
 
+def saveNum():
+    data_obj.write(str(num))
+    
+
+def getNum():
+    global num
+    num += 1
+    return num
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
+    saveNum()
+        
