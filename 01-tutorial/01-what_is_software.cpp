914854598771181e34cfe87aed1cab5dc61490bb
yhw2003 <321612813@qq.com>
2023-10-24 15:11:33 +0800

    。

---
+++ 01-tutorial/01-what_is_software.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+extern void name_handle(char *);
+
+int main() {
+    char name[50] {};
+    // 获得数据 
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    // 处理数据
+    name_handle(name);
+    // 输出数据
+    std::cout << "Hello, " << name << std::endl;
+}
+
+void name_handle(char * name) {
+    // 转化为大写
+    for (int i = 0; i < 50; i++) {
+        if (name[i] == '\0') {
+            break;
+        }
+        if (name[i] >= 'a' && name[i] <= 'z') {
+            name[i] -= 32;
+        }
+    }
+}
